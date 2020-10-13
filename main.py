9d98a2ea34e55489ce7a138e00173c4158eecd09
Houssem Zouaghi <houssem.zouaghi@ymail.com>
2020-10-12 22:22:47 -0400

    before update system

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pathManager
 import Multimedia
 from glob import glob
-
+import os
 import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input_folder", required=True,
-                help="frames folder")
-ap.add_argument("-fps", "--frame_rate", required=True,
-                help="frame rate")
-ap.add_argument("-o", "--output_file", default="./sequence.avi",
-                help="Output folder")
-args = vars(ap.parse_args())
+ap = argparse.ArgumentParser(description='Combine frames')
 
-input_folder= args['input_folder']
-output_file = args['output_file']
-fps = float(args['frame_rate'])
+ap.add_argument("-i", "--inputDir", required=True, type=str,
+                help="frames folder")
+ap.add_argument("-fps", "--frameRate", required=True, type=float,
+                help="frame rate")
+ap.add_argument("-o", "--outputDir", default="./sequence.avi", type=str,
+                help="Output folder")
+
+args = vars(ap.parse_args())
+print ("\n \n \n---------\n \n \n")
+
+
 
 #files = pathManager.find_paths(input_folder,is_sorted=True)
 
+input_folder= os.path.expanduser(args['inputDir'])
+output_file = os.path.expanduser(args['outputDir'])
+fps = float(args['frameRate'])
 files = []
 files.extend(sorted(glob(input_folder)))
 
@@ -26,5 +30,7 @@ if len(files)!=0:
     Multimedia.video_from_frames(files,output_file,fps)
 else:
     print('\n Error: No file found')
+
+
 
 
